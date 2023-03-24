83dcc686abbfe9afd5ee6fd775f89f2c1d3dc011
SimoneLollini <lollinisimone@gmail.com>
2023-03-24 14:19:37 +0100

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    //Nome repo : java-calculator-tester
+    //Scopo di questa esercitazione è di implementare i metodi di una semplice calcolatrice e di scrivere una classe di test per verificare che funzionino come ci aspettiamo! Creare una classe chiamata Calculator avente i seguenti metodi pubblici :
+    //- float add(float num1, float num2)
+    //- float subtract(float num1, float num2)
+    //- float divide(float num1, float num2)
+    //- float multiply(float num1, float num2)
+    //Creare in seguito una classe di test che abbia 4 metodi, ognuno per verificare il corretto funzionamento di ogni metodo presente nella classe Calculator.
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
